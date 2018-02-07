0fbb6a88e40224ebe1bac119b630819462c89746
jasonlam <jasonlam0619@yahoo.com>
2018-02-07 10:41:35 +0800

    eslint

--- src/mixins/_datepickerOptions.js
+++ src/mixins/_datepickerOptions.js
@@ -1,11 +1,11 @@
 export default {
-  data () {
+  data() {
     return {
       pickerOptions: {
         disabledDate(time) {
           var today = new Date()
           var yesterday = today.setDate(today.getDate() - 1)
-          return time.getTime() < yesterday;
+          return time.getTime() < yesterday
         }
       }
     }
